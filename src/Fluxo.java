87f31c6881b21ab0ed58d8eee0ad55b7e00cf786
Abner Werley <abnerwerley77@gmail.com>
2023-01-02 20:35:19 -0300

    :refactor: @abnerwerley [try/catch block refactored]

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,35 +2,28 @@
 public class Fluxo {
 
 	public static void main(String[] args) {
-		System.out.println("Início do main");
-		metodo1();
-		System.out.println("Fim do main");
+		try {
+			System.out.println("Início do main");
+			metodo1();
+			System.out.println("Fim do main");
+		} catch (NullPointerException | ArithmeticException e) {
+			System.out.println("Exception: " + e.getMessage());
+		}
 	}
 
 	private static void metodo1() {
-		try {
-			System.out.println("Início do metodo1");
-		metodo2();
+		System.out.println("Início do metodo1");
 		Conta conta = null;
 		conta.deposita();
+		metodo2();
 		System.out.println("Fim do metodo1");
-		} catch (NullPointerException e) {
-			System.out.println("NullPointerException " + e.getMessage());
-		}
-		
 	}
 
 	private static void metodo2() {
-		try {
-			System.out.println("Início do metodo2");
-			for (int i = 1; i <= 5; i++) {
-				System.out.println(i);
-				int a = i / 0;
-				System.out.println(a);
-			}
-		} catch (ArithmeticException e) {
-			System.out.println("ArithmeticException " + e.getMessage());
+		System.out.println("Início do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
 		}
-
 	}
 }
